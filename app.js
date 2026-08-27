d5645486858810dc94e0d4899f42aafe90fb397b
Madhura Thorat <madhurasthorat@gmail.com>
2026-08-27 13:36:19 +0530

    Fix MongoStore MongoDB URL

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
 const store = MongoStore.create({
-  mongoUrl: MONGO_URL,
+  mongoUrl: db_URL,
   crypto: {
     secret: process.env.SECRET,
   },
